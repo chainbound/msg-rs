f06f3ab29d56cb322141c68536bad597275cbd94
Jonas Bostoen <jonas.bostoen@hotmail.com>
2024-01-03 16:18:11 +0100

    test(transport): quic vs tcp test

---
+++ msg/examples/quic_vs_tcp.rs
@@ -0,0 +1,114 @@
+use bytes::Bytes;
+use futures::StreamExt;
+use msg_transport::{quic::Quic, Transport};
+use std::time::{Duration, Instant};
+
+use msg::{
+    tcp::{self, Tcp},
+    PubOptions, PubSocket, SubOptions, SubSocket,
+};
+
+#[tokio::main]
+async fn main() {
+    let _ = tracing_subscriber::fmt::try_init();
+
+    run_tcp().await;
+    run_quic().await;
+}
+
+async fn run_tcp() {
+    // Configure the publisher socket with options
+    let mut pub_socket = PubSocket::with_options(
+        Tcp::default(),
+        PubOptions::default()
+            .backpressure_boundary(8192)
+            .session_buffer_size(1024)
+            .flush_interval(Duration::from_micros(100)),
+    );
+
+    // Configure the subscribers with options
+    let mut sub1 = SubSocket::with_options(
+        // TCP transport with blocking connect, usually connection happens in the background.
+        Tcp::new(tcp::Config::default().blocking_connect(true)),
+        SubOptions::default().ingress_buffer_size(1024),
+    );
+
+    tracing::info!("Setting up the sockets...");
+    pub_socket
+        .bind("127.0.0.1:0".parse().unwrap())
+        .await
+        .unwrap();
+    let pub_addr = pub_socket.local_addr().unwrap();
+
+    tracing::info!("Publisher listening on: {}", pub_addr);
+
+    sub1.connect(pub_addr).await.unwrap();
+
+    sub1.subscribe("HELLO_TOPIC".to_string()).await.unwrap();
+    tracing::info!("Subscriber 1 connected and subscribed to HELLO_TOPIC");
+
+    tokio::time::sleep(Duration::from_millis(1000)).await;
+
+    run_transfer("TCP", &mut pub_socket, &mut sub1).await;
+}
+
+async fn run_quic() {
+    // Configure the publisher socket with options
+    let mut pub_socket = PubSocket::with_options(
+        Quic::default(),
+        PubOptions::default()
+            .backpressure_boundary(8192)
+            .session_buffer_size(1024)
+            .flush_interval(Duration::from_micros(100)),
+    );
+
+    // Configure the subscribers with options
+    let mut sub1 = SubSocket::with_options(
+        // TCP transport with blocking connect, usually connection happens in the background.
+        Quic::default(),
+        SubOptions::default().ingress_buffer_size(1024),
+    );
+
+    tracing::info!("Setting up the sockets...");
+    pub_socket
+        .bind("127.0.0.1:0".parse().unwrap())
+        .await
+        .unwrap();
+    let pub_addr = pub_socket.local_addr().unwrap();
+
+    tracing::info!("Publisher listening on: {}", pub_addr);
+
+    sub1.connect(pub_addr).await.unwrap();
+
+    sub1.subscribe("HELLO_TOPIC".to_string()).await.unwrap();
+    tracing::info!("Subscriber 1 connected and subscribed to HELLO_TOPIC");
+
+    tokio::time::sleep(Duration::from_millis(1000)).await;
+
+    run_transfer("QUIC", &mut pub_socket, &mut sub1).await;
+}
+
+async fn run_transfer<T: Transport + Send + Unpin + 'static>(
+    transport: &str,
+    pub_socket: &mut PubSocket<T>,
+    sub_socket: &mut SubSocket<T>,
+) {
+    let data = Bytes::from(
+        std::fs::read("./testdata/mainnetCapellaBlock7928030.ssz")
+            .expect("failed to read test file"),
+    );
+
+    let start = Instant::now();
+    pub_socket
+        .publish("HELLO_TOPIC".to_string(), data.clone())
+        .await
+        .unwrap();
+
+    tracing::info!("Published message in {:?}", start.elapsed());
+
+    let recv = sub_socket.next().await.unwrap();
+    let elapsed = start.elapsed();
+    tracing::info!("{} transfer took {:?}", transport, elapsed);
+
+    assert_eq!(recv.into_payload(), data);
+}
