0595fdd97f131c25f40fce39d703a69ba230625f
nicolas <48695862+merklefruit@users.noreply.github.com>
2024-08-25 13:31:02 +0200

    chore: lint

--- msg-sim/src/lib.rs
+++ msg-sim/src/lib.rs
@@ -1,6 +1,5 @@
 #![doc(issue_tracker_base_url = "https://github.com/chainbound/msg-rs/issues/")]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
-#![cfg_attr(not(test), warn(unused_crate_dependencies))]
 
 use std::{collections::HashMap, io, net::IpAddr, time::Duration};
 
