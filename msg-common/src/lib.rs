fec77b9b708c8ac772cc5d3a4d5dd0357ef7231d
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2025-11-16 18:39:36 +0100

    fix(quic): ipv6 support

--- msg-common/src/lib.rs
+++ msg-common/src/lib.rs
@@ -4,7 +4,10 @@
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 
-use std::time::SystemTime;
+use std::{
+    net::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6},
+    time::SystemTime,
+};
 
 use futures::future::BoxFuture;
 
@@ -32,4 +35,34 @@ pub mod constants {
     pub const KiB: u32 = 1024;
     pub const MiB: u32 = 1024 * KiB;
     pub const GiB: u32 = 1024 * MiB;
+}
+
+/// Extension trait for `SocketAddr`.
+pub trait SocketAddrExt: Sized {
+    /// Returns the unspecified IPv4 socket address, bound to port 0.
+    fn unspecified_v4() -> Self;
+
+    /// Returns the unspecified IPv6 socket address, bound to port 0.
+    fn unspecified_v6() -> Self;
+
+    /// Returns the unspecified socket address bound to port 0 of the same protocol version of
+    /// the provided address.
+    fn unspecified_compatible_with(other: &Self) -> Self;
+}
+
+impl SocketAddrExt for SocketAddr {
+    fn unspecified_v4() -> Self {
+        SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 0))
+    }
+
+    fn unspecified_v6() -> Self {
+        SocketAddr::V6(SocketAddrV6::new(Ipv6Addr::UNSPECIFIED, 0, 0, 0))
+    }
+
+    fn unspecified_compatible_with(other: &Self) -> Self {
+        match other {
+            SocketAddr::V4(_) => Self::unspecified_v4(),
+            SocketAddr::V6(_) => Self::unspecified_v6(),
+        }
+    }
 }
